e76487b0f383dfd0d1a431b6698d57bacf1eb78a
Yadnesh Kulkarni <ykulkarn@redhat.com>
2021-12-16 20:32:14 +0530

    Setup entry point to "cmdstash"
    This change allows to run commands with
    entrypoint "cmdstash"
    
    Signed-off-by: Yadnesh Kulkarni <ykulkarn@redhat.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ PBR usage https://docs.openstack.org/pbr/latest/user/using.html
 
 from setuptools import setup
 
-setup(
+setup (
+    name='cmdstash',
     setup_requires=['pbr>=2.0.0'],
     pbr=True
 )
